3798d131636058645b05ccbbac46382e2d46ac50
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-09-10 21:34:17 -0300

    atualizando consumer para visualizar headers

--- src/main/java/com/api/notificacao/domin/consumer/NotificacaoConsumer.java
+++ src/main/java/com/api/notificacao/domin/consumer/NotificacaoConsumer.java
@@ -5,6 +5,8 @@ import com.api.notificacao.domin.service.EmailService;
 import com.api.notificacao.domin.utils.JsonUitls;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Header;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +22,12 @@ public class NotificacaoConsumer {
             groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topic.notificacao-usuario}"
     )
-    public void consumerNotificacaoUsuario(String payload){
-        log.info("Evento {} recebido do topico notificacao-usuario", payload);
-        Evento evento = jsonUitls.toEvento(payload);
+    public void consumerNotificacaoUsuario(ConsumerRecord<String, String> record){
+        log.info("Evento {} recebido do topico notificacao-usuario", record);
+        Evento evento = jsonUitls.toEvento(record.value());
+        for (Header header : record.headers()) {
+            log.info("Header Key: {}, Header Value: {}", header.key(), new String(header.value()));
+        }
         emailService.enviarEmail(evento);
     }
 }
